1276cba7e894157cb47b52276329d0ab0beae3e4
Johan van Santen <johan@yellomigo.com>
2020-04-21 18:57:52 +0200

    Updates

---
+++ precache-manifest.e7fcf2818fb5428b061b391c7dd5c9dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e37f0dcb2420e9f6aae90614599a34",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "103c3b13a455967397c8",
+    "url": "/robofriends-redux/static/css/2.9dc5248e.chunk.css"
+  },
+  {
+    "revision": "a44d860c493cd1338d4d",
+    "url": "/robofriends-redux/static/css/main.bc72e3d3.chunk.css"
+  },
+  {
+    "revision": "103c3b13a455967397c8",
+    "url": "/robofriends-redux/static/js/2.df13d4ab.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/robofriends-redux/static/js/2.df13d4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44d860c493cd1338d4d",
+    "url": "/robofriends-redux/static/js/main.19ce3f21.chunk.js"
+  },
+  {
+    "revision": "031c7a520b6fbbb7b5c4",
+    "url": "/robofriends-redux/static/js/runtime-main.7719f2e8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
